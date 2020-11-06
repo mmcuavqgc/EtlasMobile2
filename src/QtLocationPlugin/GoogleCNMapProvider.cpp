c7937d3509d6412d6713694bed31b04b55641c97
ziyu <ziyu6060@163.com>
2020-11-06 11:06:16 +0800

    視頻移植，以及中文翻譯問題

---
+++ src/QtLocationPlugin/GoogleCNMapProvider.cpp
@@ -0,0 +1,224 @@
+﻿/****************************************************************************
+ *
+ * (c) 2009-2020 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#include "GoogleCNMapProvider.h"
+#if defined(DEBUG_GOOGLE_MAPS)
+#include <QFile>
+#include <QStandardPaths>
+#endif
+#include <QtGlobal>
+
+#include "QGCMapEngine.h"
+
+GoogleCNMapProvider::GoogleCNMapProvider(const QString &imageFormat, const quint32 averageSize, const QGeoMapType::MapStyle mapType, QObject* parent)
+    : MapProvider(QStringLiteral("https://www.google.cn/maps/preview"), imageFormat, averageSize, mapType, parent)
+    , _googleVersionRetrieved(false)
+    , _googleReply(nullptr)
+{
+    _versionGoogleMap       = QStringLiteral("m@218");
+    _versionGoogleSatellite = QStringLiteral("s@165");
+    _versionGoogleLabels    = QStringLiteral("h@336");
+    _versionGoogleTerrain   = QStringLiteral("t@131,r@218");
+    _versionGoogleHybrid    = QStringLiteral("y");
+    _secGoogleWord          = QStringLiteral("Galileo");
+
+}
+
+GoogleCNMapProvider::~GoogleCNMapProvider() {
+    if (_googleReply)
+        _googleReply->deleteLater();
+}
+
+//-----------------------------------------------------------------------------
+void GoogleCNMapProvider::_getSecGoogleWords(const int x, const int y, QString& sec1, QString& sec2) const {
+    sec1       = QStringLiteral(""); // after &x=...
+    sec2       = QStringLiteral(""); // after &zoom=...
+    int seclen = ((x * 3) + y) % 8;
+    sec2       = _secGoogleWord.left(seclen);
+    if (y >= 10000 && y < 100000) {
+        sec1 = QStringLiteral("&s=");
+    }
+}
+
+//-----------------------------------------------------------------------------
+void GoogleCNMapProvider::_networkReplyError(QNetworkReply::NetworkError error) {
+    qWarning() << "Could not connect to google maps. Error:" << error;
+    if (_googleReply) {
+        _googleReply->deleteLater();
+        _googleReply = nullptr;
+    }
+}
+//-----------------------------------------------------------------------------
+void GoogleCNMapProvider::_replyDestroyed() {
+    _googleReply = nullptr;
+}
+
+void GoogleCNMapProvider::_googleVersionCompleted() {
+    if (!_googleReply || (_googleReply->error() != QNetworkReply::NoError)) {
+        qDebug() << "Error collecting Google maps version info";
+        return;
+    }
+    const QString html = QString(_googleReply->readAll());
+
+#if defined(DEBUG_GOOGLE_MAPS)
+    QString filename = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
+    filename += QStringLiteral("/google.output");
+    QFile file(filename);
+    if (file.open(QIODevice::ReadWrite)) {
+        QTextStream stream(&file);
+        stream << html << endl;
+    }
+#endif
+
+    QRegExp reg(QStringLiteral("\"*https?://mt\\D?\\d..*/vt\\?lyrs=m@(\\d*)"), Qt::CaseInsensitive);
+    if (reg.indexIn(html) != -1) {
+        _versionGoogleMap = QString(QStringLiteral("m@%1")).arg(reg.capturedTexts().value(1, QString()));
+    }
+    reg = QRegExp(QStringLiteral("\"*https?://khm\\D?\\d.googleapis.cn/kh\\?v=(\\d*)"), Qt::CaseInsensitive);
+    if (reg.indexIn(html) != -1) {
+        _versionGoogleSatellite = reg.capturedTexts().value(1);
+    }
+    reg = QRegExp(QStringLiteral("\"*https?://mt\\D?\\d..*/vt\\?lyrs=t@(\\d*),r@(\\d*)"), Qt::CaseInsensitive);
+    if (reg.indexIn(html) != -1) {
+        const QStringList gc  = reg.capturedTexts();
+        _versionGoogleTerrain = QString(QStringLiteral("t@%1,r@%2")).arg(gc.value(1), gc.value(2));
+    }
+    _googleReply->deleteLater();
+    _googleReply = nullptr;
+}
+
+void GoogleCNMapProvider::_tryCorrectGoogleVersions(QNetworkAccessManager* networkManager) {
+    QMutexLocker locker(&_googleVersionMutex);
+    if (_googleVersionRetrieved) {
+        return;
+    }
+    _googleVersionRetrieved = true;
+    if (networkManager) {
+        QNetworkRequest qheader;
+        QNetworkProxy   proxy = networkManager->proxy();
+        QNetworkProxy   tProxy;
+        tProxy.setType(QNetworkProxy::DefaultProxy);
+        networkManager->setProxy(tProxy);
+        QSslConfiguration conf = qheader.sslConfiguration();
+        conf.setPeerVerifyMode(QSslSocket::VerifyNone);
+        qheader.setSslConfiguration(conf);
+        const QString url = QStringLiteral("http://maps.google.cn/maps/api/js?v=3.2&sensor=false");
+        qheader.setUrl(QUrl(url));
+        QByteArray ua;
+        ua.append(getQGCMapEngine()->userAgent());
+        qheader.setRawHeader("User-Agent", ua);
+        _googleReply = networkManager->get(qheader);
+        connect(_googleReply, &QNetworkReply::finished, this, &GoogleCNMapProvider::_googleVersionCompleted);
+        connect(_googleReply, &QNetworkReply::destroyed, this, &GoogleCNMapProvider::_replyDestroyed);
+        connect(_googleReply, QOverload<QNetworkReply::NetworkError>::of(&QNetworkReply::error), this, &GoogleCNMapProvider::_networkReplyError);
+        networkManager->setProxy(proxy);
+    }
+}
+
+QString GoogleCNStreetMapProvider::_getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) {
+    // http://mt1.google.com/vt/lyrs=m
+    QString server  = QStringLiteral("mt");
+    QString request = QStringLiteral("vt");
+    QString sec1; // after &x=...
+    QString sec2; // after &zoom=...
+    _getSecGoogleWords(x, y, sec1, sec2);
+    _tryCorrectGoogleVersions(networkManager);
+    return QString(QStringLiteral("http://%1%2.google.cn/%3/lyrs=%4&hl=%5&x=%6%7&y=%8&z=%9&s=%10"))
+        .arg(server)
+        .arg(_getServerNum(x, y, 4))
+        .arg(request)
+        .arg(_versionGoogleMap)
+        .arg(_language)
+        .arg(x)
+        .arg(sec1)
+        .arg(y)
+        .arg(zoom)
+        .arg(sec2);
+}
+
+QString GoogleCNSatelliteMapProvider::_getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) {
+    // http://mt1.google.com/vt/lyrs=s
+    QString server  = QStringLiteral("khm");
+    QString request = QStringLiteral("kh");
+    QString sec1; // after &x=...
+    QString sec2; // after &zoom=...
+    _getSecGoogleWords(x, y, sec1, sec2);
+    _tryCorrectGoogleVersions(networkManager);
+    return QString(QStringLiteral("http://%1%2.google.cn/%3/v=%4&hl=%5&x=%6%7&y=%8&z=%9&s=%10"))
+        .arg(server)
+        .arg(_getServerNum(x, y, 4))
+        .arg(request)
+        .arg(_versionGoogleSatellite)
+        .arg(_language)
+        .arg(x)
+        .arg(sec1)
+        .arg(y)
+        .arg(zoom)
+        .arg(sec2);
+}
+
+QString GoogleCNLabelsMapProvider::_getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) {
+    QString server  = "mts";
+    QString request = "vt";
+    QString sec1; // after &x=...
+    QString sec2; // after &zoom=...
+    _getSecGoogleWords(x, y, sec1, sec2);
+    _tryCorrectGoogleVersions(networkManager);
+    return QString(QStringLiteral("http://%1%2.google.cn/%3/lyrs=%4&hl=%5&x=%6%7&y=%8&z=%9&s=%10"))
+        .arg(server)
+        .arg(_getServerNum(x, y, 4))
+        .arg(request)
+        .arg(_versionGoogleLabels)
+        .arg(_language)
+        .arg(x)
+        .arg(sec1)
+        .arg(y)
+        .arg(zoom)
+        .arg(sec2);
+}
+
+QString GoogleCNTerrainMapProvider::_getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) {
+    QString server  = QStringLiteral("mt");
+    QString request = QStringLiteral("vt");
+    QString sec1; // after &x=...
+    QString sec2; // after &zoom=...
+    _getSecGoogleWords(x, y, sec1, sec2);
+    _tryCorrectGoogleVersions(networkManager);
+    return QString(QStringLiteral("http://%1%2.google.cn/%3/v=%4&hl=%5&x=%6%7&y=%8&z=%9&s=%10"))
+        .arg(server)
+        .arg(_getServerNum(x, y, 4))
+        .arg(request)
+        .arg(_versionGoogleTerrain)
+        .arg(_language)
+        .arg(x)
+        .arg(sec1)
+        .arg(y)
+        .arg(zoom)
+        .arg(sec2);
+}
+
+QString GoogleCNHybridMapProvider::_getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) {
+    QString server = QStringLiteral("mt");
+    QString request = QStringLiteral("vt");
+    QString sec1; // after &x=...
+    QString sec2; // after &zoom=...
+    _getSecGoogleWords(x, y, sec1, sec2);
+    _tryCorrectGoogleVersions(networkManager);
+    return QString(QStringLiteral("http://%1%2.google.cn/%3/lyrs=%4&hl=%5&x=%6%7&y=%8&z=%9&s=%10"))
+        .arg(server)
+        .arg(_getServerNum(x, y, 4))
+        .arg(request)
+        .arg(_versionGoogleHybrid)
+        .arg(_language)
+        .arg(x)
+        .arg(sec1)
+        .arg(y)
+        .arg(zoom)
+        .arg(sec2);
+}
---
+++ src/QtLocationPlugin/GoogleCNMapProvider.h
@@ -0,0 +1,134 @@
+﻿/****************************************************************************
+ *
+ * (c) 2009-2020 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+ *
+ * QGroundControl is licensed according to the terms in the file
+ * COPYING.md in the root of the source code directory.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include "MapProvider.h"
+#include <QNetworkReply>
+#include <QMutex>
+
+class GoogleCNMapProvider : public MapProvider {
+    Q_OBJECT
+
+public:
+    GoogleCNMapProvider(const QString& imageFormat, const quint32 averageSize,
+                      const QGeoMapType::MapStyle _mapType, QObject* parent = nullptr);
+
+    ~GoogleCNMapProvider();
+
+    // Google Specific private slots
+private slots:
+    void _networkReplyError(QNetworkReply::NetworkError error);
+    void _googleVersionCompleted();
+    void _replyDestroyed();
+
+protected:
+    // Google Specific private methods
+    void _getSecGoogleWords(const int x, const int y, QString& sec1, QString& sec2) const;
+    void _tryCorrectGoogleVersions(QNetworkAccessManager* networkManager);
+
+    // Google Specific attributes
+    bool           _googleVersionRetrieved;
+    QNetworkReply* _googleReply;
+    QMutex         _googleVersionMutex;
+    QString        _versionGoogleMap;
+    QString        _versionGoogleSatellite;
+    QString        _versionGoogleLabels;
+    QString        _versionGoogleTerrain;
+    QString        _versionGoogleHybrid;
+    QString        _secGoogleWord;
+};
+
+// NoMap = 0,
+// StreetMap,
+// SatelliteMapDay,
+// SatelliteMapNight,
+// TerrainMap,
+// HybridMap,
+// TransitMap,
+// GrayStreetMap,
+// PedestrianMap,
+// CarNavigationMap,
+// CycleMap,
+// CustomMap = 100
+
+static const quint32 AVERAGE_GOOGLECN_STREET_MAP  = 4913;
+static const quint32 AVERAGE_GOOGLECN_SAT_MAP     = 56887;
+static const quint32 AVERAGE_GOOGLECN_TERRAIN_MAP = 19391;
+
+// -----------------------------------------------------------
+// Google Street Map
+
+class GoogleCNStreetMapProvider : public GoogleCNMapProvider {
+    Q_OBJECT
+
+public:
+    GoogleCNStreetMapProvider(QObject* parent = nullptr)
+        : GoogleCNMapProvider(QStringLiteral("png"), AVERAGE_GOOGLECN_STREET_MAP, QGeoMapType::StreetMap, parent) {}
+
+protected:
+     QString _getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
+};
+
+// -----------------------------------------------------------
+// Google Street Map
+
+class GoogleCNSatelliteMapProvider : public GoogleCNMapProvider {
+    Q_OBJECT
+
+public:
+    GoogleCNSatelliteMapProvider(QObject* parent = nullptr)
+        : GoogleCNMapProvider(QStringLiteral("jpg"), AVERAGE_GOOGLECN_SAT_MAP,
+                            QGeoMapType::SatelliteMapDay, parent) {}
+
+protected:
+    QString _getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
+};
+
+// -----------------------------------------------------------
+// Google Labels Map
+
+class GoogleCNLabelsMapProvider : public GoogleCNMapProvider {
+    Q_OBJECT
+
+public:
+    GoogleCNLabelsMapProvider(QObject* parent = nullptr)
+        : GoogleCNMapProvider(QStringLiteral("png"), AVERAGE_TILE_SIZE, QGeoMapType::CustomMap, parent) {}
+
+protected:
+    QString _getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
+};
+
+// -----------------------------------------------------------
+// Google Terrain Map
+
+class GoogleCNTerrainMapProvider : public GoogleCNMapProvider {
+    Q_OBJECT
+
+public:
+    GoogleCNTerrainMapProvider(QObject* parent = nullptr)
+        : GoogleCNMapProvider(QStringLiteral("png"), AVERAGE_GOOGLECN_TERRAIN_MAP, QGeoMapType::TerrainMap, parent) {}
+
+protected:
+    QString _getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
+};
+
+// -----------------------------------------------------------
+// Google Hybrid Map
+
+class GoogleCNHybridMapProvider : public GoogleCNMapProvider {
+    Q_OBJECT
+
+public:
+    GoogleCNHybridMapProvider(QObject* parent = nullptr)
+        : GoogleCNMapProvider(QStringLiteral("png"), AVERAGE_GOOGLECN_SAT_MAP, QGeoMapType::HybridMap, parent) {}
+
+protected:
+    QString _getURL(const int x, const int y, const int zoom, QNetworkAccessManager* networkManager) override;
+};
